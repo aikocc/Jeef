a634d29e516c734ce04ea6c3b31b610abe7795dc
Joshua Busacker <me@aiko.cc>
2021-01-01 12:08:08 +1100

    fixed the right quotes this time

--- cogs/youtube_feed.py
+++ cogs/youtube_feed.py
@@ -39,7 +39,7 @@ class YoutubeFeed(utils.Cog):
                 for x in new_videos:
                 	video_links.append(x)
 
-            message = f"Woah {new_videos[0]["snippet"]["channelTitle"]} has just uploaded a new video, you should go check it out!\n\n{'\n'.join(video_links)}"
+            message = f"Woah {new_videos[0]['snippet']['channelTitle']} has just uploaded a new video, you should go check it out!\n\n{'\n'.join(video_links)}"
                 
             await self.bot.get_guild(793352612568760320).get_channel(794347469483671564).send(message)
 
