7577737f5074a2380bb7b3c7a505d09e285a01a0
Joshua Busacker <me@aiko.cc>
2021-01-01 13:48:52 +1100

    Indentation can suck my right nut too

--- cogs/message_blacklist.py
+++ cogs/message_blacklist.py
@@ -10,49 +10,49 @@ class MessageBlacklist(utils.Cog):
 
     @blacklist.command()
     async def add(self, ctx: utils.Context, word: str):
-    	"""Add words to blacklist"""
-		
-		# Create a database connection and insert the word into the database        
+        """Add words to blacklist"""
+        
+        # Create a database connection and insert the word into the database        
         async with self.bot.database() as db:
-        	data = await db("INSERT INTO blacklist_words (guild_id, word) VALUES ($1, $2)", message.guild.id, word)
+            data = await db("INSERT INTO blacklist_words (guild_id, word) VALUES ($1, $2)", message.guild.id, word)
 
         await message.channel.send(f"Added `{word}` to the blacklist.")
 
     @blacklist.command()
     async def remove(self, ctx: utils.Context, word: str):
-    	"""removes words to blacklist"""
-		
-		# Create a database connection and insert the word into the database        
+        """removes words to blacklist"""
+        
+        # Create a database connection and insert the word into the database        
         async with self.bot.database() as db:
-        	data = await db("DELETE FROM blacklist_words WHERE guild_id = $1, word = $2", message.guild.id, word)
+            data = await db("DELETE FROM blacklist_words WHERE guild_id = $1, word = $2", message.guild.id, word)
 
         await message.channel.send(f"Removed `{word}` to the blacklist.")
 
     @utils.Cog.listener()
     async def on_message(self, message: utils.Message):
-    	"""Check all messages and check if message contains a blacklisted word"""
+        """Check all messages and check if message contains a blacklisted word"""
 
-    	# Start a database connection and grab all blacklisted words
-    	async with self.bot.database() as db:
-        	data = await db("SELECT word FROM blacklist_words WHERE guild_id = $1", message.guild.id)
+        # Start a database connection and grab all blacklisted words
+        async with self.bot.database() as db:
+            data = await db("SELECT word FROM blacklist_words WHERE guild_id = $1", message.guild.id)
 
         # Check if there are any blacklisted works
         if len(data) == 0:
-        	return
+            return
 
         blacklisted_words = []
 
         # Turn database result to a list
         for x in data:
-        	blacklisted_words.append(x["word"])
+            blacklisted_words.append(x["word"])
 
         msg = message.content.strip(list(string.punctuation + string.digits + " "))
 
         # Check if blacklisted word is in message
-       	for word in blacklisted_words:
-       		if word in msg:
-       			await message.delete()
-       			await message.channel.send(f"{message.author} you can't say that word! >:(")
+        for word in blacklisted_words:
+            if word in msg:
+                await message.delete()
+                await message.channel.send(f"{message.author} you can't say that word! >:(")
 
 
 
