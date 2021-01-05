e4a45ff43ead5abe792b1e88b5453e9c2f9333dd
Joshua Busacker <me@aiko.cc>
2021-01-05 22:23:24 +1100

    indents yaY

--- cogs/verification_handler.py
+++ cogs/verification_handler.py
@@ -6,9 +6,10 @@ import random
 
 class VerificationHandler(utils.Cog):
 
-	def __init__(self, bot):
-		self.bot = bot
-		self.random_code = dict()
+    def __init__(self, bot):
+        self.bot = bot
+        self.random_code = dict()
+
 
     @utils.Cog.listener()
     async def on_member_join(self, member: discord.Member):
@@ -16,37 +17,37 @@ class VerificationHandler(utils.Cog):
         Member Join Handler
         """
         if member.guild.id != 795795423838470186:
-        	return
+            return
 
         code = "".join([random.choice(list(string.ascii_letters + string.digits + ' ')) for _ in range(10)])
 
         self.random_code[member.id] = code
 
         welcome_message = textwrap.dedent(f"""
-        	Welcome to `{member.guild.name}`.
+            Welcome to `{member.guild.name}`.
 
-        	To help fight against bots and automated raids we have implemented a verification system.
-        	To verify you'll need to `.verify <code>` without the <>.
+            To help fight against bots and automated raids we have implemented a verification system.
+            To verify you'll need to `.verify <code>` without the <>.
 
-        	Your Code is: `{self.random_code[member.id]}`
+            Your Code is: `{self.random_code[member.id]}`
         """)
 
         await member.send(welcome_message)
 
     @utils.command()
     async def verify(self, ctx: utils.Context, code: str):
-    	"""
-    	Verify Command
-    	"""
+        """
+        Verify Command
+        """
 
-    	if self.random_code[ctx.author.id] == code:
-    		del self.random_code[ctx.author.id]
-    		guild = self.bot.get_guild(795795423838470186)
-    		member = guild.get_member(ctx.author.id)
-    		try:
-    			await member.add_roles(guild.get_role(795796913508974622))
-    		except Exception as e:
-    			raise e
+        if self.random_code[ctx.author.id] == code:
+            del self.random_code[ctx.author.id]
+            guild = self.bot.get_guild(795795423838470186)
+            member = guild.get_member(ctx.author.id)
+            try:
+                await member.add_roles(guild.get_role(795796913508974622))
+            except Exception as e:
+                raise e
 
 def setup(bot:utils.Bot):
     x = VerificationHandler(bot)
